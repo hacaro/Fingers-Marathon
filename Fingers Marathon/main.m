15c8d9d304363085faced43908616594cc09f9d6
hacaro <hacaro76@gmail.com>
2013-10-30 09:03:17 +0100

    Initial Commit

---
+++ Fingers Marathon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fingers Marathon
+//
+//  Created by Massimo Scalia on 30/10/13.
+//  Copyright hacaro 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
